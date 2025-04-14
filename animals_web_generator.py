ab99fc36ec310d53be4659e9b9434f6f39912efa
Richard Ludwig <vladtepesch@Mac.fritz.box>
2025-04-14 14:23:08 +0200

    added prey to animal card, error handling inside funktion

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,6 @@
 import json
 
+
 def load_data(file_path):
     """ Loads a JSON file """
     with open(file_path, "r") as handle:
@@ -11,25 +12,38 @@ def load_template(file_path):
     with open(file_path, "r") as gettemp:
         return gettemp.read()
 
+
+def serialue_animal(fox):
+    output = ""
+    output += "<li class='cards__item'>\n"
+    output += f"<div class='card__title'>{fox["name"]}<br/>\n</div>"
+    try:
+        output += f"<p class='card__text'>\n <strong>Diet: </strong>{fox["characteristics"]["diet"]}<br/>\n"
+    except KeyError:
+        pass
+    try:
+        output += f"<strong>Prey: </strong>{fox["characteristics"]["prey"]}<br/>\n"
+    except KeyError:
+        pass
+    output += "<strong>Location: </strong>"
+    output += f" {", ".join(fox["locations"])}<br/>\n"
+    try:
+        output += f"<strong>Type: </strong>{fox["characteristics"]["type"]}<br/>\n"
+    except KeyError:
+        pass
+    output += "</p>"
+    output += "</li>"
+    return output
+
+
 animals_data = load_data("animals_data.json")
 template = load_template("animals_template.html")
 
 output = ""
 for fox in animals_data:
-    try:
-        output += "<li class='cards__item'>\n"
-        output += f"<div class='card__title'>{fox["name"]}<br/>\n</div>"
-        output += f"<p class='card__text'>\n <strong>Diet: </strong>{fox["characteristics"]["diet"]}<br/>\n"
-        output += "<strong>Location: </strong>"
-        output += f" {", ".join(fox["locations"])}<br/>\n"
-        output += f"<strong>Type: </strong>{fox["characteristics"]["type"]}<br/>\n"
-        output += "</p>"
-        output += "</li>"
-    except KeyError:
-        output += "\n"
-        continue
+    output += serialue_animal(fox)
+
 
 temp_with_data = template.replace("__REPLACE_ANIMALS_INFO__", output)
-print(temp_with_data)
 with open("animals.html", "w") as makepage:
     makepage.write(temp_with_data)
